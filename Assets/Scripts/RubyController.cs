fb504598d7b60ad2f6ef311b48521c22cb3f231b
Aliuem <126677273+Aliuem@users.noreply.github.com>
2023-04-05 01:57:27 +0100

    2. Adicionar um componente 2D de corpo rígido

--- Assets/Scripts/RubyController.cs
+++ Assets/Scripts/RubyController.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Rubycontroller : MonoBehaviour
-{
+{  
+
+      Rigidbody2D rigidbody2d;
+    float horizontal; 
+    float vertical;
+
+
     // Start is called before the first frame update
     void Start()
-    {
-        
+
+    {  
+         rigidbody2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
